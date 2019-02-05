562d49469cf4007dcb8f7d8a8bed27232dc2c882
annasla95 <annaslabuszewska@gmail.com>
2019-02-05 21:28:13 +0100

    PROMETHEUS GRAFANA # sending data to prometheus

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ var url = require('url');
 
 var AWS = require('aws-sdk');
 const Influx = require('influxdb-nodejs');
+var prometheus = require('prom-client');
 
 var config = require('./hyperflowMonitoringEcsPlugin.config.js');
 
+var prometheusPushGateway = new prometheus.Pushgateway(config.prometheusPushGatewaway);
+
 var MonitoringEcsPlugin = function () {
 };
+
+var prometheusMetrics = {};
 
 MonitoringEcsPlugin.prototype.storeEcsData = function()
 {
@@ -26,11 +31,15 @@ MonitoringEcsPlugin.prototype.writeDataToDatabase=function(metric, data)
     data["hfId"] = that.getHfId();
 
     client.write(metric)
-    .field(data)
-    .then(() => true)
-    .catch(console.error);
+        .field(data)
+        .then(() => true)
+.catch(console.error);
 }
 
+MonitoringEcsPlugin.prototype.writeToPrometheus = function(metric, value, labels) {
+    metric.set({wfId: that.getWfId(), hfId: that.getHfId(), ...labels}, value);
+    prometheusPushGateway.push({jobName: 'hyperflow-ecs-monitoring-plugin'}, () => {});
+}
 
 MonitoringEcsPlugin.prototype.getEcsData = function()
 {
@@ -42,76 +51,108 @@ MonitoringEcsPlugin.prototype.getEcsData = function()
     var cloudwatch = new AWS.CloudWatch(configAws);
 
     var dataToStore ={};
-    
+
     var params = {cluster:config.clusterName};
     ecs.listContainerInstances(params, function(err, data) {
         if (err) console.log(err, err.stack);
         else
         {
-          containerCount=data.containerInstanceArns.length;
+            containerCount=data.containerInstanceArns.length;
 
-          that.writeDataToDatabase("hyperflow_ecs_monitor_container",{containerInstanceCount:containerCount})
+            that.writeDataToDatabase("hyperflow_ecs_monitor_container",{containerInstanceCount:containerCount})
+
+            prometheusMetrics.hyperflow_ecs_monitor_container = prometheusMetrics.hyperflow_ecs_monitor_container ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_ecs_monitor_container',
+                    help: 'containerInstanceCount',
+                    labelNames: ['wfId', 'hfId']
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_ecs_monitor_container, containerCount);
         }
     });
 
     ecs.listTasks(params, function(err, data) {
-        if (err) console.log(err, err.stack); 
+        if (err) console.log(err, err.stack);
         else{
-           taskCount=data.taskArns.length;
+            taskCount=data.taskArns.length;
 
-          that.writeDataToDatabase("hyperflow_ecs_monitor_tasks",{tasksCount: taskCount})
+            that.writeDataToDatabase("hyperflow_ecs_monitor_tasks",{tasksCount: taskCount})
+
+            prometheusMetrics.hyperflow_ecs_monitor_tasks = prometheusMetrics.hyperflow_ecs_monitor_tasks ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_ecs_monitor_tasks',
+                    help: 'tasksCount',
+                    labelNames: ['wfId', 'hfId']
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_ecs_monitor_tasks, taskCount);
         }
     });
 
     cloudwatch.waitFor('alarmExists', function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else{
-          AlarmLowValue = data.MetricAlarms[0].StateValue;
-          AlarmHightValue = data.MetricAlarms[1].StateValue;
-          that.writeDataToDatabase("hyperflow_ecs_monitor_alarms",{alarmLowValue: AlarmLowValue, alarmHightValue:AlarmHightValue});
+            AlarmLowValue = data.MetricAlarms[0].StateValue;
+            AlarmHightValue = data.MetricAlarms[1].StateValue;
+            that.writeDataToDatabase("hyperflow_ecs_monitor_alarms",{alarmLowValue: AlarmLowValue, alarmHightValue:AlarmHightValue});
+
+            prometheusMetrics.hyperflow_ecs_monitor_alarms = prometheusMetrics.hyperflow_ecs_monitor_alarms ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_ecs_monitor_alarms',
+                    help: 'alarms',
+                    labelNames: ['wfId', 'hfId', 'alarmLowValue', 'alarmHighValue']
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_ecs_monitor_alarms, 0, {alarmLowValue: AlarmLowValue, alarmHighValue: AlarmHightValue});
         }
     });
 
     //cloudwatch.
     var endTime = new Date();
     var startTime=new Date(endTime - 1000000);
-    
+
 
     console.log(startTime);
-    
+
     console.log(endTime);
-    
-      var paramsCpuCluster = {
+
+    var paramsCpuCluster = {
         EndTime: endTime, /* required */
         MetricName: "CPUUtilization", /* required */
         Namespace: "AWS/ECS", /* required */
         Period: 60, /* required */
         StartTime: startTime, /* required */
         Dimensions: [
-          {
-             "Name": "ClusterName",
-             "Value": "ecs_test_cluster_hyperflow"
-          }
-          /* more items */
+            {
+                "Name": "ClusterName",
+                "Value": "ecs_test_cluster_hyperflow"
+            }
+            /* more items */
         ],
 
         Statistics: [
-           "Average"
+            "Average"
         ],
-      };
+    };
 
 
-      cloudwatch.getMetricStatistics(paramsCpuCluster, function(err, data) {
+    cloudwatch.getMetricStatistics(paramsCpuCluster, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else
         {
             var value = data.Datapoints[data.Datapoints.length-1].Average
-            console.log(data.Datapoints[data.Datapoints.length-1]);   
+            console.log(data.Datapoints[data.Datapoints.length-1]);
             that.writeDataToDatabase("hyperflow_cluster_cpu",{ precentageCPU:  value});
-        }
-      });
 
-      var paramsCpuWorkers = {
+            prometheusMetrics.hyperflow_cluster_cpu = prometheusMetrics.hyperflow_cluster_cpu ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_cluster_cpu',
+                    help: 'percentageCPU',
+                    labelNames: ['wfId', 'hfId'],
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_cluster_cpu, value);
+        }
+    });
+
+    var paramsCpuWorkers = {
         EndTime: endTime, /* required */
         MetricName: "CPUUtilization", /* required */
         Namespace: "AWS/ECS", /* required */
@@ -126,28 +167,36 @@ MonitoringEcsPlugin.prototype.getEcsData = function()
                 "Name": "ClusterName",
                 "Value": "ecs_test_cluster_hyperflow"
             }
-          /* more items */
+            /* more items */
         ],
 
         Statistics: [
-           "Average"
+            "Average"
         ],
-      };
+    };
 
 
-      cloudwatch.getMetricStatistics(paramsCpuWorkers, function(err, data) {
+    cloudwatch.getMetricStatistics(paramsCpuWorkers, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else
         {
-                    // successful response
+            // successful response
             var value = data.Datapoints[data.Datapoints.length-1].Average
-            console.log(data.Datapoints[data.Datapoints.length-1]);   
+            console.log(data.Datapoints[data.Datapoints.length-1]);
             that.writeDataToDatabase("hyperflow_worker_cpu",{ precentageCPU:  value});
+
+            prometheusMetrics.hyperflow_worker_cpu = prometheusMetrics.hyperflow_worker_cpu ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_worker_cpu',
+                    help: 'percentageCPU',
+                    labelNames: ['wfId', 'hfId'],
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_worker_cpu, value);
         }
-      });
+    });
 
 
-      var paramsCpuMaster = {
+    var paramsCpuMaster = {
         EndTime: endTime, /* required */
         MetricName: "CPUUtilization", /* required */
         Namespace: "AWS/ECS", /* required */
@@ -162,40 +211,48 @@ MonitoringEcsPlugin.prototype.getEcsData = function()
                 "Name": "ClusterName",
                 "Value": "ecs_test_cluster_hyperflow"
             }
-          /* more items */
+            /* more items */
         ],
 
         Statistics: [
-           "Average"
+            "Average"
         ],
-      };
+    };
 
 
-      cloudwatch.getMetricStatistics(paramsCpuMaster, function(err, data) {
+    cloudwatch.getMetricStatistics(paramsCpuMaster, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else
         {
-                    // successful response
+            // successful response
             var value = data.Datapoints[data.Datapoints.length-1].Average
             //var time =  new Date(data.Datapoints[data.Datapoints.length-1].Timestamp)
-            console.log(data.Datapoints[data.Datapoints.length-1]);   
+            console.log(data.Datapoints[data.Datapoints.length-1]);
             that.writeDataToDatabase("hyperflow_master_cpu",{ precentageCPU:  value});
+
+            prometheusMetrics.hyperflow_master_cpu = prometheusMetrics.hyperflow_master_cpu ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_master_cpu',
+                    help: 'percentageCPU',
+                    labelNames: ['wfId', 'hfId'],
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_master_cpu, value);
         }
-      });
+    });
 
 ///////////////////////////////////////////////////////////////
-      var paramsCpuCluster = {
+    var paramsCpuCluster = {
         EndTime: endTime, /* required */
         MetricName: "MemoryUtilization", /* required */
         Namespace: "AWS/ECS", /* required */
         Period: 60, /* required */
         StartTime: startTime, /* required */
         Dimensions: [
-          {
-             "Name": "ClusterName",
-             "Value": "ecs_test_cluster_hyperflow"
-          }
-          /* more items */
+            {
+                "Name": "ClusterName",
+                "Value": "ecs_test_cluster_hyperflow"
+            }
+            /* more items */
         ],
 
         Statistics: [
@@ -204,17 +261,25 @@ MonitoringEcsPlugin.prototype.getEcsData = function()
       };
 
 
-      cloudwatch.getMetricStatistics(paramsCpuCluster, function(err, data) {
+    cloudwatch.getMetricStatistics(paramsCpuCluster, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else
         {
             var value = data.Datapoints[data.Datapoints.length-1].Average
-            console.log(data.Datapoints[data.Datapoints.length-1]);   
+            console.log(data.Datapoints[data.Datapoints.length-1]);
             that.writeDataToDatabase("hyperflow_cluster_memory",{ precentageCPU:  value});
-        }
-      });
 
-      var paramsCpuWorkers = {
+            prometheusMetrics.hyperflow_cluster_memory = prometheusMetrics.hyperflow_cluster_memory ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_cluster_memory',
+                    help: 'percentage memory',
+                    labelNames: ['wfId', 'hfId'],
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_cluster_memory, value);
+        }
+    });
+
+    var paramsCpuWorkers = {
         EndTime: endTime, /* required */
         MetricName: "MemoryUtilization", /* required */
         Namespace: "AWS/ECS", /* required */
@@ -229,28 +294,36 @@ MonitoringEcsPlugin.prototype.getEcsData = function()
                 "Name": "ClusterName",
                 "Value": "ecs_test_cluster_hyperflow"
             }
-          /* more items */
+            /* more items */
         ],
 
         Statistics: [
-           "Average"
+            "Average"
         ],
-      };
+    };
 
 
-      cloudwatch.getMetricStatistics(paramsCpuWorkers, function(err, data) {
+    cloudwatch.getMetricStatistics(paramsCpuWorkers, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else
         {
-                    // successful response
+            // successful response
             var value = data.Datapoints[data.Datapoints.length-1].Average
-            console.log(data.Datapoints[data.Datapoints.length-1]);   
+            console.log(data.Datapoints[data.Datapoints.length-1]);
             that.writeDataToDatabase("hyperflow_worker_memory",{ precentageCPU:  value});
+
+            prometheusMetrics.hyperflow_worker_memory = prometheusMetrics.hyperflow_worker_memory ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_worker_memory',
+                    help: 'percentage memory',
+                    labelNames: ['wfId', 'hfId'],
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_worker_memory, value);
         }
-      });
+    });
 
 
-      var paramsCpuMaster = {
+    var paramsCpuMaster = {
         EndTime: endTime, /* required */
         MetricName: "MemoryUtilization", /* required */
         Namespace: "AWS/ECS", /* required */
@@ -265,26 +338,34 @@ MonitoringEcsPlugin.prototype.getEcsData = function()
                 "Name": "ClusterName",
                 "Value": "ecs_test_cluster_hyperflow"
             }
-          /* more items */
+            /* more items */
         ],
 
         Statistics: [
-           "Average"
+            "Average"
         ],
-      };
+    };
 
 
-      cloudwatch.getMetricStatistics(paramsCpuMaster, function(err, data) {
+    cloudwatch.getMetricStatistics(paramsCpuMaster, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else
         {
-                    // successful response
+            // successful response
             var value = data.Datapoints[data.Datapoints.length-1].Average
             //var time =  new Date(data.Datapoints[data.Datapoints.length-1].Timestamp)
-            console.log(data.Datapoints[data.Datapoints.length-1]);   
+            console.log(data.Datapoints[data.Datapoints.length-1]);
             that.writeDataToDatabase("hyperflow_master_memory",{ precentageCPU:  value});
+
+            prometheusMetrics.hyperflow_master_memory = prometheusMetrics.hyperflow_master_memory ||
+                new prometheus.Gauge({
+                    name: 'hyperflow_master_memory',
+                    help: 'percentage memory',
+                    labelNames: ['wfId', 'hfId'],
+                });
+            that.writeToPrometheus(prometheusMetrics.hyperflow_master_memory, value);
         }
-      });
+    });
 }
 
 
